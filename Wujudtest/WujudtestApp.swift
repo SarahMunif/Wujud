710f73e18ea1625cfab64897a97e6172f2d6bb21
Sarah <monifsarah@gmail.com>
2025-01-22 21:12:03 +0300

    Initial Commit

---
+++ Wujudtest/WujudtestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WujudtestApp.swift
+//  Wujudtest
+//
+//  Created by su on 22/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct WujudtestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WujudtestTests/WujudtestTests.swift
@@ -0,0 +1,16 @@
+//
+//  WujudtestTests.swift
+//  WujudtestTests
+//
+//  Created by su on 22/01/2025.
+//
+
+import Testing
+
+struct WujudtestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
